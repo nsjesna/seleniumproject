fffcb8d6cc9c51294d0818a26e02c01b16881292
Devi <devisklm89@gmail.com>
2025-08-25 11:00:11 +0530

    Commit by Devi

--- src/githubproject/Githubproject.java
+++ src/githubproject/Githubproject.java
@@ -14,6 +14,7 @@ public class Githubproject {
 		System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");
 		WebDriver driver=new ChromeDriver();
 		driver.get("http://www.facebook.com");
+		System.out.println("Hi");
 		
 
 	}
